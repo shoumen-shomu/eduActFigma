5b0eb5da0d3917bd117d4f61873e42e07b8497df
Sijan666 <666majharulislam@gmail.com>
2026-02-11 05:25:05 +0600

    feat : add responsive and commenting in discover section

--- src/components/sections/Discover.jsx
+++ src/components/sections/Discover.jsx
@@ -8,15 +8,19 @@ const Discover = () => {
     return (
         <>
         <section className='discover'>
-            <div className="">
-                <Container className={"bg-[url('/src/assets/discoverBg.png')] bg-center bg-cover bg-no-repeat py-20 lg:w-330 px-30"}>
-                    <Commonh3 commonh3={'Discover the campus through a video tour'} className={'leading-10 w-132 text-white pb-10'}/>
-                    <div className="relative">
+            <Container className="lg:w-330">
+                <div className={"bg-[url('/src/assets/discoverBg.png')] bg-center bg-cover bg-no-repeat py-20 lg:px-30 px-5"}>
+                    {/* common tag */}
+                    <Commonh3 commonh3={'Discover the campus through a video tour'} className={'leading-10 lg:w-132 text-white lg:pb-10 pb-3 text-center lg:text-left'}/>
+                    {/* common tag */}
+                    {/* button */}
+                    <div className="relative text-center lg:text-left">
                         <Button btnText={'Discover More'} className={'py-5 pr-14 pl-6 bg-secondary rounded-md text-white text-[18px] font-bold relative'}/>
-                        <FaArrowRightLong className='text-white font-bold absolute top-1/2 left-37 -translate-y-1/2'/>
+                        <FaArrowRightLong className='text-white font-bold absolute top-1/2 lg:left-37 left-60 -translate-y-1/2'/>
                     </div>
-                </Container>
-            </div>
+                    {/* button */}
+                </div>
+            </Container>
         </section>
         </>
     )
