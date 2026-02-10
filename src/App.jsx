5c4225b5a0af904fd6f76c9ce71e6fdd5e45f702
menabil <nabil99cc@gmail.com>
2026-02-10 23:21:01 +0600

    feat(header): add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import './App.css'
-import Home from './pages/Home'
-import { Routes , Route } from 'react-router-dom'
-import RootLayouts from './components/layouts/RootLayouts'
-import About from './pages/About'
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import RootLayouts from "./components/layouts/RootLayouts";
+import About from "./pages/About";
 
-
-function App () {
+function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-          <Route path="/about" element={<About/>} />
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,12 +1,64 @@
-
-
+import Container from "../Container";
+import Flex from "../Flex";
+import Images from "../Images";
+import logo from "/src/assets/logo.png";
+import { FiSearch } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa";
+import { FiPhoneCall } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa6";
 
 const Header = () => {
-    return (
-        <>
-        <h3>Header</h3>
-        </>
-    )
-}
+  return (
+    <>
+      <Container className={"py-2"}>
+        <Flex>
+          <Images imgSrc={logo} />
+          <ul className="flex items-center gap-x-[55px] font-semibold text-[#2A254D] text-base ml-[115px] mr-16">
+            <li>
+              <Link to={""}>Home</Link>
+            </li>
+            <li>
+              <Link to={""} className=" flex items-center gap-x-1">
+                About <FaAngleDown />
+              </Link>
+            </li>
+            <li>
+              <Link to={""}>Courses</Link>
+            </li>
+            <li>
+              <Link to={""}>Pages </Link>
+            </li>
+            <li>
+              <Link to={""}> Blog</Link>
+            </li>
+            <li>
+              <Link to={""}> Shop</Link>
+            </li>
+          </ul>
+          <Flex className={"gap-x-3 text-[#2A254D] text-xl"}>
+            <Link to={""}>
+              <FiSearch />
+            </Link>
+            <Link to={""}>
+              <FaRegUser />
+            </Link>
+          </Flex>
+          <div className="bg-primary py-4 pl-4 pr-12.5 rounded-tl-full rounded-bl-full ml-25">
+            <Flex className={"gap-x-4"}>
+              <div className="text-primary bg-white text-[28px] p-3 rounded-full">
+                <FiPhoneCall />
+              </div>
+              <div className="text-white font-semibold ">
+                <p className="text-[18px]">(303) 555-0105</p>
+                <p className="text-sm">Call to Questions</p>
+              </div>
+            </Flex>
+          </div>
+        </Flex>
+      </Container>
+    </>
+  );
+};
 
-export default Header
+export default Header;
