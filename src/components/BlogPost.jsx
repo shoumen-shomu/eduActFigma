26f4f11e6d15b6ad74b2bf9d0061ab10be008428
menabil <nabil99cc@gmail.com>
2026-02-11 19:00:14 +0600

    feat(blogPost): add blogPost component

---
+++ src/components/BlogPost.jsx
@@ -0,0 +1,38 @@
+import { FaArrowRight } from "react-icons/fa";
+import Flex from "./Flex";
+
+const BlogPost = () => {
+  return (
+    <div className="bg-[#152B4A] h-[488px] w-[416px] rounded-[14px] relative group">
+      <div className="absolute w-[357px] bg-white -bottom-20 left-1/2 -translate-x-1/2 shadow-newMade p-7 rounded-tl-[10px] rounded-tr-[10px] rounded-bl-[50px] rounded-br-[50px]">
+        <Flex className={"gap-x-25"}>
+          <p className="text-sm text-white bg-primary font-semibold py-2 px-2.5 rounded-[5px]">
+            Development
+          </p>
+          <p className="text-base text-secondary font-semibold">26 Mar, 2023</p>
+        </Flex>
+        <h5 className="py-6 text-2xl text-[#2A254D] font-bold">
+          The Complete Web Developer Guideline 2023
+        </h5>
+        <Flex className={"justify-between"}>
+          <div className="flex items-center">
+            <div className="h-11 w-11 rounded-full bg-[#152B4A] mr-2.5"></div>
+            <div className="">
+              <p className="text-base text-[#2A254D] font-bold pb-1">
+                Darrell Steward
+              </p>
+              <p className="text-xs text-[#697585] font-medium tracking-[4%] uppercase">
+                Fronted Developer
+              </p>
+            </div>
+          </div>
+          <div className="w-10 h-10 rounded-full bg-transparent group-hover:bg-secondary border border-secondary text-[#697585] group-hover:text-white duration-300 relative">
+            <FaArrowRight className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+          </div>
+        </Flex>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
